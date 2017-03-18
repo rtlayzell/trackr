635a2ff719ce76eadadd4e159bf7be635389650f
Reegan Layzell <reegan.layzell@live.co.uk>
2017-03-18 01:20:20 +0000

    Add project files.

---
+++ src/Trackr.Framework/Mementor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trackr.Framework
+{
+	public interface IMemento
+	{
+
+	}
+
+	public class CloneableObjectMemento : IMemento
+	{
+
+	}
+
+	public class PropertyMemento : IMemento
+	{
+
+	}
+
+
+	public class Mementor
+	{
+	}
+}
---
+++ tests/Trackr.Framework.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Trackr.Framework.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Trackr.Framework.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("827e974d-08ec-404b-ad32-da4b38e3b8bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
